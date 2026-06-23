b2eb64a1ed8f90a0e6e8588305572fc500e8af52
LNicolasOnera <lucas.nicolas@onera.fr>
2026-06-23 16:00:36 +0200

    Récupération du code fait par B.Pons

---
+++ src/pymodaq_plugins_inficon/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Template.py
@@ -0,0 +1,211 @@
+import numpy as np
+from pymodaq_utils.utils import ThreadCommand
+from pymodaq_data.data import DataToExport
+from pymodaq_gui.parameter import Parameter
+from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
+from pymodaq.utils.data import DataFromPlugins
+from pymodaq_plugins_inficon.hardware.STM2_Python_Wrapper import InficonSTM2
+
+
+class DAQ_0DViewer_Inficon_STM2(DAQ_Viewer_base):
+    """ Module class for Inficon STM-2 instrument.
+        It needs the Inficon driver in order to communicate with PyMoDAQ.
+        It has been tested only with Inficon STM-2 rate/thickness monitor.
+    =======================================================================
+    Attributes:
+    -----------
+    controller: object
+        InficonSTM2 object from python wrapper STM2_Serial_Communication that uses SerialBaseSMDP methods to communicate
+        with the instrument according to the specific protocol (Sycon Multi Drop Protocol) described in its documentation.
+
+    ========================================================================
+
+    """
+
+    params = comon_parameters + [
+        {'title': 'Device serial number :', 'name': 'device_serial_number', 'type': 'list'},
+        {'title': 'Device information :', 'name': 'device_info', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Crystal status :', 'name': 'crystal_status', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Crystal life (%) :', 'name': 'crystal_life', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Timer (H:MM:SS) :', 'name': 'timer', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Set default parameters :', 'name': 'set_default_parameters', 'type': 'bool'},
+        {'title': 'Zeroes thickness :', 'name': 'set_thickness_zeroes', 'type': 'bool'},
+        {'title': 'Zeroes timer :', 'name': 'set_timer_zeroes', 'type': 'bool'},
+        {'title': 'Film name :', 'name': 'film_name', 'type': 'str'},
+        {'title': 'Film density :', 'name': 'film_density', 'type': 'float', 'max': 99.99, 'min': 0.40},
+        {'title': 'Film Z-ratio :', 'name': 'film_zratio', 'type': 'float', 'max': 9.999, 'min': 0.100},
+        {'title': 'Film tooling (%) :', 'name': 'film_tooling', 'type': 'float', 'max': 999.9, 'min': 10.0},
+        {'title': 'Samples number :', 'name': 'samples_number', 'type': 'int', 'max': 50, 'min': 1}
+    ]
+
+    def link_ports_and_sn(self):
+        list_serial_numbers = []
+        if self.stm2_ports:
+            for port in self.stm2_ports:
+                serial_number = InficonSTM2(port).get_serial_number()
+                list_serial_numbers.append(serial_number + ' (' + port + ')')
+        self.settings.child('device_serial_number').setLimits(list_serial_numbers)
+
+    def ini_attributes(self):
+        self.controller: InficonSTM2 = None
+        self.controller = None
+        self.stm2_ports = InficonSTM2().stm2_ports
+        self.port = None
+        self.port_change = False
+        self.link_ports_and_sn()
+        self.emit_status(ThreadCommand('Update_Status', ['Detected STM-2 COM ports : ' + str(self.stm2_ports), 'log']))
+
+    def commit_settings(self, param: Parameter):
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            Device serial number : list of detected STM-2 monitors, can be selected during initialization
+            (changes may occur between units, see red light on it).
+            Set default parameters : sets the unit to its constructor default values.
+            Zeroes thickness : as in title.
+            Zeroes timer : as in title.
+            Film name : sets unit film name (8 characters max), nice to remember who is who.
+            Film density : sets film density on the unit for calculated values.
+            Film Z-ratio : sets film Z-ratio on the unit for calculated values.
+            Film tooling : sets film tooling on the unit for calculated values.
+            Samples number : sets sample number on the unit for calculated values.
+        """
+        try:
+            if param.name() == 'device_serial_number':
+                old_port = self.port
+                self.port = param.value()[-5:-1]
+                if old_port != self.port:
+                    self.port_change = True
+                    self.controller = InficonSTM2(self.port)
+                else:
+                    self.port_change = False
+                self.emit_status(ThreadCommand('Update_Status', ["Selected port is now : " + str(self.port), 'log']))
+            elif param.name() == 'set_default_parameters':
+                self.controller.set_default_parameters()
+            elif param.name() == 'set_thickness_zeroes':
+                self.controller.set_thickness_zeroes()
+            elif param.name() == 'set_timer_zeroes':
+                self.controller.set_timer_zeroes()
+            elif param.name() == 'film_name':
+                self.controller.set_film_name(param.value())
+            elif param.name() == 'film_density':
+                self.controller.set_film_density(param.value())
+            elif param.name() == 'film_zratio':
+                self.controller.set_film_zratio(param.value())
+            elif param.name() == 'film_tooling':
+                self.controller.set_film_tooling(param.value())
+            elif param.name() == 'samples_number':
+                self.controller.set_samples_number(param.value())
+            self.update_parameter_branch()
+        except Exception as e:
+            self.emit_status(ThreadCommand('Update_Status', [str(e), 'log']))
+
+    def update_parameter_branch(self):
+        infos = ('Model and firmware version : {}, Build type : {}, Firmware CRC : {}, Reset Status : {}'.format
+                 (self.controller.get_infos(), self.controller.get_build_type(), self.controller.get_firmware_crc(),
+                  self.controller.get_reset_status()))
+        self.settings.child('device_info').setValue(infos)
+        self.settings.child('crystal_status').setValue(self.controller.get_cristal_status())
+        self.settings.child('crystal_life').setValue(self.controller.get_cristal_life())
+        self.settings.child('timer').setValue(self.controller.get_timer())
+        self.settings.child('film_name').setValue(self.controller.get_film_name())
+        self.settings.child('film_density').setValue(self.controller.get_film_density())
+        self.settings.child('film_zratio').setValue(self.controller.get_film_zratio())
+        self.settings.child('film_tooling').setValue(self.controller.get_film_tooling())
+        self.settings.child('samples_number').setValue(self.controller.get_samples_number())
+
+    def ini_detector(self, controller=None):
+        """Detector communication initialization
+
+        Parameters
+        ----------
+        controller: (object)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
+            (Master case)
+
+        Returns
+        -------
+        info: str
+        initialized: bool
+            False if initialization failed otherwise True
+        """
+        self.ini_detector_init(slave_controller=controller)
+        if (self.stm2_ports != []) & self.is_master:
+            if not self.port_change:
+                self.port = self.stm2_ports[0]
+            self.controller = InficonSTM2(self.port)
+        self.update_parameter_branch()
+        self.dte_signal_temp.emit(DataToExport('STM-2 Data',
+                                               data=[DataFromPlugins(name='STM-2 Frequency',
+                                                                     data=[np.array([0, 5])],
+                                                                     dim='Data0D',
+                                                                     labels=['Frequency (Hz)']),
+                                                     DataFromPlugins(name='STM-2 Thickness',
+                                                                     data=[np.array([0, 5])],
+                                                                     dim='Data0D',
+                                                                     labels=['Thickness (Å)']),
+                                                     DataFromPlugins(name='STM-2 Film mass',
+                                                                     data=[np.array([0, 5])],
+                                                                     dim='Data0D',
+                                                                     labels=['Film mass (µg/cm²)']),
+                                                     DataFromPlugins(name='STM-2 Rate',
+                                                                     data=[np.array([0, 5])],
+                                                                     dim='Data0D',
+                                                                     labels=['Rate (Å/s)']),
+                                                     DataFromPlugins(name='STM-2 Mass accumulation rate',
+                                                                     data=[np.array([0, 5])],
+                                                                     dim='Data0D',
+                                                                     labels=['Mass accumulation rate (μg/(*s/cm²))'])]))
+        info = "Default values for selected STM-2 should be printed and graphs should appear."
+        initialized = bool(self.controller)
+        return info, initialized
+
+    def close(self):
+        """Terminate the communication protocol"""
+        self.controller = None
+
+    def grab_data(self, Naverage=1, **kwargs):
+        """Start a grab from the detector
+
+        Parameters
+        ----------
+        Naverage: int
+            Number of hardware averaging (if hardware averaging is possible, self.hardware_averaging should be set to
+            True in class preamble and you should code this implementation)
+        kwargs: dict
+            others optionals arguments
+        """
+        self.dte_signal.emit(DataToExport('STM-2 Data',
+                                          data=[DataFromPlugins(name='STM-2 Frequency',
+                                                                data=[np.array([self.controller.get_frequency()])],
+                                                                dim='Data0D',
+                                                                labels=['Frequency (Hz)']),
+                                                DataFromPlugins(name='STM-2 Thickness',
+                                                                data=[np.array([self.controller.get_thickness()])],
+                                                                dim='Data0D',
+                                                                labels=['Thickness (Å)']),
+                                                DataFromPlugins(name='STM-2 Film mass',
+                                                                data=[np.array([self.controller.get_film_mass()])],
+                                                                dim='Data0D',
+                                                                labels=['Film mass (µg/cm²)']),
+                                                DataFromPlugins(name='STM-2 Rate',
+                                                                data=[np.array([self.controller.get_rate()])],
+                                                                dim='Data0D',
+                                                                labels=['Rate (Å/s)']),
+                                                DataFromPlugins(name='STM-2 Mass accumulation rate',
+                                                                data=[np.array(
+                                                                    [self.controller.get_mass_accumulation_rate()])],
+                                                                dim='Data0D',
+                                                                labels=['Mass accumulation rate (μg/(*s/cm²))'])]))
+
+    def stop(self):
+        """Stop the current grab hardware wise if necessary"""
+        self.emit_status(ThreadCommand('Update_Status', ['Stopped']))
+        ##############################
+        return ''
+
+
+if __name__ == '__main__':
+    main(__file__)
---
+++ src/pymodaq_plugins_inficon/hardware/STM2_Python_Wrapper.py
@@ -0,0 +1,221 @@
+import serial.tools.list_ports
+
+class SerialBaseSMDP :
+
+    ADR = [0x22]
+
+    def __init__(self):
+        self.stm2_ports = []
+
+    def search_stm2_ports(self):
+        ports = serial.tools.list_ports.comports()
+        for port in ports:
+            if port.manufacturer == 'Silicon Laboratories':
+                infos = InficonSTM2(port.device).get_infos()
+                if 'STM-2' in str(infos):
+                    self.stm2_ports.append(port.device)
+        if not self.stm2_ports:
+            raise ConnectionError("STM-2 not found, check connections and driver installation.")
+
+    def send_command(self, port, cmd):
+        cmd_bytes = bytes(cmd, encoding='utf-8')
+        if cmd in ['0', '@', 'P', "'", 'p']:
+            cmd_sent = bytes([0x02] + self.ADR) + cmd_bytes + self.checksum(bytes(self.ADR) + cmd_bytes) + bytes([0x0D])
+        else:
+            cmd_sent = bytes([0x02] + self.ADR + [0x80]) + cmd_bytes + self.checksum(bytes(self.ADR + [0x80]) + cmd_bytes) + bytes([0x0D])
+        try:
+            with serial.Serial(port, baudrate=115200, timeout=1) as s:
+                s.write(cmd_sent)
+                resp_bytes = s.read_until(bytes([0x0D]))
+                return self.check_response(cmd_bytes, resp_bytes)
+        except serial.SerialException as e:
+            raise ConnectionError(f'Serial error : {e}')
+        except Exception as e:
+            raise Exception(f'Unexpected error : {e}')
+
+    def check_response(self, cmd_bytes, resp_bytes) :
+        if resp_bytes:
+            header = resp_bytes[:2]
+            if not (header == bytes(([0x02] + self.ADR))):
+                raise ConnectionError(f"Wrong header received : {header}, expected header : {bytes(([0x02] + self.ADR))}.")
+            crc = resp_bytes[-3:-1]
+            crc_calculated = self.checksum(resp_bytes[1:-3])
+            if not crc == crc_calculated:
+                raise ConnectionError(f"Wrong checksum received : {crc}, checksum calculated with received data : {crc_calculated}.")
+            resp_status_bytes = resp_bytes[2]
+            self.resp_status(cmd_bytes[0], resp_status_bytes)
+            return resp_bytes[3:-3].decode()
+        else:
+            raise ConnectionError('No response received.')
+
+    @staticmethod
+    def checksum(data):
+        ck = sum(data) % 256
+        checksum_1 = (ck >> 4) + 0x30
+        checksum_2 = (ck & 0xF) + 0x30
+        return bytes((checksum_1, checksum_2))
+
+    @staticmethod
+    def resp_status(cmd_bytes, resp_status_bytes):
+        error_code = resp_status_bytes - cmd_bytes
+        if error_code > 7:
+#            print("STM-2 has been reset since last power flag acknowledgement.")
+            error_code += -8
+        elif error_code == 1:
+#            print("Command understood and executed.")
+            pass
+        elif error_code == 2:
+            raise ConnectionError(f"Illegal command (command code not valid) : {cmd_bytes}.")
+        elif error_code == 3:
+            raise ConnectionError(f"Syntax error (too many bytes in data field, not enough bytes) for command {cmd_bytes}.")
+        elif error_code == 4:
+            raise ConnectionError(f"Data range error for command : {cmd_bytes}.")
+        elif error_code == 5:
+            raise ConnectionError(f"Command {cmd_bytes} inhibited.")
+        elif error_code == 6:
+            raise ConnectionError(f"{cmd_bytes} : obsolete command, no action taken.")
+        else:
+            raise ConnectionError(f"Response status for command : {cmd_bytes} not listed : {error_code}.")
+
+class InficonSTM2:
+
+    def __init__(self, port = None):
+        self.protocol = SerialBaseSMDP()
+        if not port:
+            self.protocol.search_stm2_ports()
+            self.stm2_ports = self.protocol.stm2_ports
+        else:
+            self.port = port
+
+    def get_infos(self):
+        """Returns instrument model and firmware version."""
+        return self.protocol.send_command(self.port, '@')
+
+    def get_serial_number(self):
+        """Returns instrument serial number."""
+        return self.protocol.send_command(self.port, '@A')
+
+    def get_build_type(self):
+        """Returns instrument build type."""
+        return self.protocol.send_command(self.port, '@B')
+
+    def reset_flags(self):
+        """Acknowledge "a" response, reset flags so response to 'a' poll is "@" next time."""
+        return self.protocol.send_command(self.port, 'L')
+
+    def set_default_parameters(self):
+        """Set parameters to default values. Clears memory, reboots module, sets power loss and memloss flags."""
+        return self.protocol.send_command(self.port, 'b')
+
+    def reset(self):
+        """Cause reset."""
+        return self.protocol.send_command(self.port, 'd')
+
+    def get_firmware_crc(self):
+        """Queries firmware version CRC as a decimal value."""
+        return self.protocol.send_command(self.port, 'p')
+
+    def set_timer_thickness_zeroes(self):
+        """Zeroes timer and thickness."""
+        return self.protocol.send_command(self.port, 'B')
+
+    def set_thickness_zeroes(self):
+        """Zeroes thickness."""
+        return self.protocol.send_command(self.port, 'D')
+
+    def set_timer_zeroes(self):
+        """Zeroes timer."""
+        return self.protocol.send_command(self.port, 'C')
+
+    def set_film_name(self, name):
+        """Sets the current film name to 'name'."""
+        if not len(name) < 9:
+            raise ValueError(f"Names should have 8 or less characters, name entered : {name}, length : {len(name)}.")
+        return self.protocol.send_command(self.port, 'q=' + name)
+
+    def get_film_name(self):
+        """Returns current film name."""
+        return self.protocol.send_command(self.port, 'q?')
+
+    def set_film_density(self, density):
+        """Sets current film density to 'density'."""
+        if not 0.40 <= density <= 99.99:
+            raise ValueError(f"Density value should be between 0.40 and 99.99; value entered : {density}.")
+        return self.protocol.send_command(self.port, 'E=' + str(density))
+
+    def get_film_density(self):
+        """Returns current film density."""
+        return self.protocol.send_command(self.port, 'E?')
+
+    def set_film_zratio(self, zratio):
+        """Sets current film Z-ratio to 'Zratio'."""
+        if not 0.100 <= zratio <= 9.999:
+            raise ValueError(f"Z-ratio value should be between 0.100 and 9.999; value entered : {zratio}.")
+        return self.protocol.send_command(self.port, 'F=' + str(zratio))
+
+    def get_film_zratio(self):
+        """Returns current film Z-ratio."""
+        return self.protocol.send_command(self.port, 'F?')
+
+    def set_film_tooling(self, filmtooling):
+        """Set current film tooling to 'FilmTooling'."""
+        if not 10.0 <= filmtooling <= 999.9:
+            raise ValueError(
+                f"Film tooling value should be between 10.0 and 999.9; value entered : {filmtooling}.")
+        return self.protocol.send_command(self.port, 'J=' + str(filmtooling))
+
+    def get_film_tooling(self) -> str:
+        """Returns current film tooling."""
+        return self.protocol.send_command(self.port, 'J?')
+
+    def set_samples_number(self, number):
+        """Sets number of samples to 'number' for temporal averaging."""
+        if not 1 <= number <= 50:
+            raise ValueError(f"Samples number value should be between 1 and 50; value entered : {number}.")
+        return self.protocol.send_command(self.port, 'r=' + str(number))
+
+    def get_samples_number(self):
+        """Returns current sample number."""
+        return self.protocol.send_command(self.port, 'r?')
+
+    def get_cristal_status(self):
+        """Return crystal fail status : @ = crystal good, ! = crystal failed."""
+        status = self.protocol.send_command(self.port, 'M')
+        if status == '@':
+            return 'crystal good'
+        elif status == '!':
+            return 'crystal failed'
+        else :
+            return status
+
+    def get_thickness(self):
+        """Return thickness value."""
+        return float(self.protocol.send_command(self.port, 'S'))
+
+    def get_film_mass(self):
+        """Return film mass."""
+        return float(self.protocol.send_command(self.port, 's'))
+
+    def get_rate(self):
+        """Return rate."""
+        return float(self.protocol.send_command(self.port, 'T'))
+
+    def get_mass_accumulation_rate(self):
+        """Return mass accumulation rate in μg/ (*s/cm²)."""
+        return float(self.protocol.send_command(self.port, 't'))
+
+    def get_frequency(self):
+        """Return sensor frequency."""
+        return float(self.protocol.send_command(self.port, 'U'))
+
+    def get_cristal_life(self):
+        """Return crystal life."""
+        return self.protocol.send_command(self.port, 'V')
+
+    def get_timer(self) -> str:
+        """Return timer in H:MM:SS format."""
+        return self.protocol.send_command(self.port, 'W')
+
+    def get_reset_status(self) -> str:
+        """Return RESET Status : @ = OK, A = lost power, D = lost NONV memory, E = lost power and NONV memory."""
+        return self.protocol.send_command(self.port, 'a')
